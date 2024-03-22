53ad65603cc73958ff41ed6d8fe3d18733c19d82
sedin-vasanthakumar <vasanthakumar@sedintechnologies.com>
2024-03-23 01:29:58 +0530

    Imported react in importmaps

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,9 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "react" # @18.2.0
+pin "react-dom" # @18.2.0
+pin "scheduler" # @0.23.0
+pin "react-router-dom" # @6.22.3
+pin "@remix-run/router", to: "@remix-run--router.js" # @1.15.3
+pin "react-router" # @6.22.3
